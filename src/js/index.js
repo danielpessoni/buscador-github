00f00424088e494bbf61b25dd543ab80c9059689
Daniel Pessoni <daniel.pessoni@outlook.com>
2026-07-15 17:17:10 -0300

    feat: Adiciona a busca pelos dados de repositório quando o usuário apertar "Enter"

--- src/js/index.js
+++ src/js/index.js
@@ -5,22 +5,33 @@ const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.getElementById('profile-results');
 
-btnSearch.addEventListener('click', async () => {
+const searchUser = async () => {
     const userName = inputSearch.value;
+    
     if (!userName) {
         alert('Por favor, digite um nome de usuário do GitHub.');
         profileResults.innerHTML = "";
         return;
     }
+
     profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
+    
     try {
         const userData = await fetchGitHubUser(userName);
         const userRepos = await fetchGitHubUserRepos(userName);
         renderProfile(userData, userRepos, profileResults);
-        console.log(userRepos);
     } catch (error) {
         console.error('Erro ao buscar o perfil do usuário:', error);
         alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
         profileResults.innerHTML = "";
+    }
+};
+
+btnSearch.addEventListener('click', searchUser);
+
+inputSearch.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        searchUser();
     }
 });
